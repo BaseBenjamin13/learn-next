e06b0c2eac4b1562a673aac01399f972fcc9bcf3
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-01-27 18:28:23 -0500

    made loading component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,14 @@ import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
+import Loader from '../components/Loader'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div>
+        <Loader show />
         <Link prefetch={false} href={{
             pathname: '/[username]',
             query: { username: 'billybean'}
