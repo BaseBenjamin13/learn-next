d0a18a069d281daf707e2c1c8f2909de31a28a63
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-01-27 20:56:26 -0500

    using react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import NavBar from '@/components/Navbar';
+import { Toaster } from 'react-hot-toast'
 
 import { GetServerSideProps } from 'next';
 
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <>
             <NavBar />
             <Component {...pageProps} />
+            <Toaster />
         </>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 import Loader from '../components/Loader'
+import toast from 'react-hot-toast'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return (
     <div>
         <Loader show />
+
+        <button onClick={() => toast.success('hello toast!')}>
+            Toast Me
+        </button>
+
         <Link prefetch={false} href={{
             pathname: '/[username]',
             query: { username: 'billybean'}
