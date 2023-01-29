c884d0559ae99bc1ab9c51d5dac8f3713bbe5d00
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-01-28 21:30:31 -0500

    logging in with google auth

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,8 +1,35 @@
 
+import { auth, googleAuthProvider } from '../lib/firebase';
+
 export default function EnterPage({ }) {
+
+    const user = null;
+    const username = null;
+
     return (
         <main>
-            <h1>Sign Up</h1>
+            {user ? 
+                !username ? <UsernameForm /> : <SignOutButton /> 
+                : 
+                <SignInButton />
+            }
         </main>
     )
+}
+
+function SignInButton() {
+    const signInWithGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider);
+    }
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <img src={'/google.png'} /> Sign in with Google
+        </button>
+    )
+}
+function SignOutButton() {
+    return <button onClick={() => auth.signOut()}>Sign Out</button>
+}
+function UsernameForm() {
+
 }
